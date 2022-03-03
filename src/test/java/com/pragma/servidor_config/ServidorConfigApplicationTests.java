38b4fd6c2333ff5b76b2aa71635d3728dd15bde3
Jonatancon <jhonatanlora2011@gmail.com>
2022-03-03 08:59:06 -0500

    initial commit

---
+++ src/test/java/com/pragma/servidor_config/ServidorConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pragma.servidor_config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServidorConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
